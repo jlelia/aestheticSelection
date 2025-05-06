c6dde7f942a7ff3399e2c1c0433110a780818a6e
James L. Elia <james.elia@yale.edu>
2025-05-06 14:56:24 -0400

    Removed test scripts. Renamed Cluster.py to SDXL.py

--- cudaTest.py
+++
@@ -1,5 +0,0 @@
-import torch
-print("CUDA available:", torch.cuda.is_available())
-print("CUDA version seen by PyTorch:", torch.version.cuda)
-x = torch.rand(2, device="cuda")
-print("Device for x:", x.device)
